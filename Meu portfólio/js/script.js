fa82da69016b17a074637edc9e74e37bc93d190c
Lucas Zorzete dos Reis <lucasreis.gamer2008@gmail.com>
2025-04-29 20:51:43 -0300

    Atualizado

--- Meu portfólio/js/script.js
+++ Meu portfólio/js/script.js
@@ -7,7 +7,7 @@ function openCode() {
 }
 
 function openSite() {
-    location.href = "https://sparkly-youtiao-0e0b0b.netlify.app/"
+    location.href = "https://coruscating-shortbread-da0110.netlify.app/"
 }
 
 function closeSidebar() {
